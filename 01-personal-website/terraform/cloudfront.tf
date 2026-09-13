4fa920d71878e939e4416bc0e3818261291b511b
Aliaa Gamal <alliaagamall@gmail.com>
2026-09-14 00:40:33 +0300

    feat(cloudfront): configure secure distribution

--- 01-personal-website/terraform/cloudfront.tf
+++ 01-personal-website/terraform/cloudfront.tf
@@ -5,3 +5,59 @@ resource "aws_cloudfront_origin_access_control" "website" {
   signing_behavior                  = "always"
   signing_protocol                  = "sigv4"
 }
+
+resource "aws_cloudfront_distribution" "website" {
+  enabled             = true
+  comment             = "${local.name_prefix} CloudFront distribution"
+  default_root_object = "index.html"
+  is_ipv6_enabled = true
+
+  origin {
+    domain_name              = aws_s3_bucket.website.bucket_regional_domain_name
+    origin_id                = "s3-${aws_s3_bucket.website.id}"
+    origin_access_control_id = aws_cloudfront_origin_access_control.website.id
+  }
+
+  default_cache_behavior {
+    target_origin_id       = "s3-${aws_s3_bucket.website.id}"
+    viewer_protocol_policy = "redirect-to-https"
+
+    allowed_methods = [
+      "GET",
+      "HEAD"
+    ]
+
+    cached_methods = [
+      "GET",
+      "HEAD"
+    ]
+
+    compress = true
+
+    forwarded_values {
+      query_string = false
+
+      cookies {
+        forward = "none"
+      }
+    }
+  }
+
+  restrictions {
+    geo_restriction {
+      restriction_type = "none"
+    }
+  }
+
+  viewer_certificate {
+    cloudfront_default_certificate = true
+    minimum_protocol_version       = "TLSv1.2_2021"
+  }
+
+  tags = merge(
+    local.common_tags,
+    {
+      Component = "content-delivery"
+    }
+  )
+}
