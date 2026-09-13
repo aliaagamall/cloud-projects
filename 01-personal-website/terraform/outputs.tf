283c62790f863ca2ae77698990f503c06e75cf50
Aliaa Gamal <alliaagamall@gmail.com>
2026-09-14 00:50:39 +0300

    feat(cloudfront): expose distribution outputs

--- 01-personal-website/terraform/outputs.tf
+++ 01-personal-website/terraform/outputs.tf
@@ -1,3 +1,4 @@
+# s3 outputs
 output "website_bucket_name" {
   description = "Name of the private S3 bucket used for website assets."
   value       = aws_s3_bucket.website.bucket
@@ -6,4 +7,15 @@ output "website_bucket_name" {
 output "website_bucket_arn" {
   description = "ARN of the private S3 bucket used for website assets."
   value       = aws_s3_bucket.website.arn
+}
+
+cloudfront outputs
+output "cloudfront_distribution_id" {
+  description = "ID of the CloudFront distribution."
+  value       = aws_cloudfront_distribution.website.id
+}
+
+output "cloudfront_domain_name" {
+  description = "Domain name of the CloudFront distribution."
+  value       = aws_cloudfront_distribution.website.domain_name
 }
